0325dad49a334f98c547ca4b97302ff40fe16045
Jerry Janer <janerjerry99@gmail.com>
2021-04-18 00:08:34 +0800

    Validating Field Part 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // Variable
- const sendBtn = document.getElementById('sendBtn');
+ const sendBtn = document.getElementById('sendBtn'),
+       email = document.getElementById('email'),
+       subject = document.getElementById('subject'),
+       message = document.getElementById('message');
 
 // Event Listeners
 eventListeners();
@@ -7,11 +10,22 @@ eventListeners();
 function eventListeners(){
     // App Init
     document.addEventListener('DOMContentLoaded',appInit);
+
+    // Validate the form
+    email.addEventListener('blur',validateField);
+    subject.addEventListener('blur',validateField);
+    message.addEventListener('blur',validateField);
+
 }
-
-
 
 // function
 function appInit(){
     sendBtn.disabled = true;
+}
+// Validate  Fields
+function validateField(){
+    let errors;
+
+    // Validate the Length of the Field
+    validateLength(this)
 }
